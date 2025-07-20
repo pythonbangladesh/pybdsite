6ff3610a11e78b658857a31bdd5e2ddd5271f134
taufiq-ai <taufiqkhantusar@gmail.com>
2025-07-20 07:23:43 +0600

    PyBDsite - add coc and contact form

--- src/app.py
+++ src/app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template
+from flask import redirect
+
 import os
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
@@ -10,9 +14,17 @@ def home():
     except Exception as e:
         return f"Error: {str(e)}", 500
 
+
 @app.route('/healthz')
 def health():
     return "OK", 200
+
+
+@app.route('/coc')
+def coc():
+    external_coc = 'https://github.com/pythonbangladesh/public-docs/blob/main/code-of-conduct.md'
+    return redirect(external_coc)
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))  # Default to 10000 for Render
